5677284488668088776e978a53941e7bff47f56c
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-16 10:33:25 +0530

    swap the varivle multiple method

---
+++ JsEssentials/swapNumber.js
@@ -0,0 +1,21 @@
+let a=9
+let b=10
+
+// let c=[a,b]=[b,a]
+// console.log(c)
+
+
+// a=a+b //19
+// b=a-b //19-10=9
+// a=a-b //19-9=10
+
+// console.log("a",a)
+// console.log("b",b)
+
+
+let temp=a //temp=9
+a=b //a=9
+b=temp
+
+console.log("a",a)
+console.log('b',b)
