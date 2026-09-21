fb521414129682ce902b03bcefb8f7e942c3e00e
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-21 08:48:55 +0530

    isbn number

--- loops/isbn.js
+++ loops/isbn.js
@@ -1,25 +1,24 @@
 let prompt=require("prompt-sync")()
 
-let num=Number(prompt("Enter a number-: "))
+let num=Number(prompt("enter the number-: "))
 let temp=num
 let count=0
-
 
 while(temp>0){
     count++
     temp=Math.floor(temp/10)
 }
 
-if(count!=10){
-    console.log("not isbn number")
-}else{
-    let ans=0
-    while(num>0){
+if(count!=10) console.log("Not isbn Number")
+else {
+     let ans=0  
+      while(num>0){
         let digit=num%10
         ans=ans+(digit*count)
         count--
         num=Math.floor(num/10)
     }
 
-    console.log(ans%11==0 ?"Valid isbn number ":"not valid isbn number")
+    console.log(ans%11==0 ?"Valid Isbn Number":"Not Valid isbn")
+
 }
