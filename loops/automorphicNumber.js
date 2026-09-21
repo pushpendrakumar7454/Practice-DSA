51efffa766ec99af494da388022a68d58b6c01df
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-21 08:21:40 +0530

    automorphic

--- loops/automorphicNumber.js
+++ loops/automorphicNumber.js
@@ -1,14 +1,14 @@
 const prompt=require("prompt-sync")()
-
-let num=Number(prompt("Enter a num-: "))
+let num=Number(prompt("Enter a number-: "))
 let sq=num*num
 let temp=num
 let digit=1
+
+
 while(temp>0){
     digit=digit*10
     temp=Math.floor(temp/10)
 }
 
-if(sq%digit===num){
-    console.log("automorphic")
-}else console.log("not automorphic")
+if(sq%digit==num) console.log("Automorphic number")
+    else console.log("Not a AutoMorphic Number")
