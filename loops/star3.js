dc4bdc20937c969721895b65e9d2798fdea30179
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-23 07:24:17 +0530

    v star

---
+++ loops/star3.js
@@ -0,0 +1,13 @@
+const promt = require("prompt-sync")();
+
+let n = Number(promt("Enter the your number-: "));
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    process.stdout.write(" ");
+  }
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+  console.log();
+}
