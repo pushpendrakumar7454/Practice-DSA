325dafead3753fe922eaaedcf44b39d6d6884327
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-23 08:33:15 +0530

    reverse array

--- array/reversethearray.js
+++ array/reversethearray.js
@@ -1,3 +1,4 @@
+//first approach
 // let arr=[4,5,6,2,45]
 
 // let rev=[]
@@ -7,14 +8,31 @@
 // }
 
 // console.log(rev)
+  
 
-let arr=[5,6,8,4,2]
-let temp=new Array(arr.length)
-let i=arr.length-1
+//second approach
+// let arr=[5,6,8,4,2]
+// let temp=new Array(arr.length)
+// let i=arr.length-1
 
-for(let j=0;j<arr.length;j++){
-    temp[j]=arr[i]
-    i--
+// for(let j=0;j<arr.length;j++){
+//     temp[j]=arr[i]
+//     i--
+// }
+
+// console.log(temp)
+
+//third approach
+let arr=[34,4,6,888,4,3]
+let i=0;
+let j=arr.length-1
+
+while(i<j){
+    let temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+    j--
+    i++
 }
 
-console.log(temp)
+console.log(arr)
